f639b5508bd375eba5cfdc5406d1613c97bc5995
Joanna <astronautnemo@gmail.com>
2023-04-15 14:05:09 +0300

    created Charity components using scaffold

--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :charities
   resources :administrators
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- Backend/db/schema.rb
+++ Backend/db/schema.rb
@@ -10,12 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_15_105520) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_15_110339) do
   create_table "administrators", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "email"
     t.string "password"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "charities", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password"
+    t.text "description"
+    t.string "img_url"
+    t.integer "goal_amount"
+    t.integer "total_donations"
+    t.integer "admin_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
