ccea0edbfdbecb93be6d34d58a1eb648f937c7c4
Joanna <astronautnemo@gmail.com>
2023-04-15 14:01:49 +0300

    created the Administrator components using scaffold

---
+++ Backend/db/migrate/20230415105520_create_administrators.rb
@@ -0,0 +1,12 @@
+class CreateAdministrators < ActiveRecord::Migration[7.0]
+  def change
+    create_table :administrators do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :password
+
+      t.timestamps
+    end
+  end
+end
