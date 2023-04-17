e36ede74a8b0fc4e60f38c43a01ad2a4f63e002f
Glen <glen.kimani@student.moringaschool.com>
2023-04-17 21:51:02 +0300

    Commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,22 @@
 import React, { useState } from "react";
 // import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
 import { Login } from "./components/Login";
 import { SignUp } from "./components/SignUp"
 
 function App() {
   return (
+    <NavBar>
+    <Footer>
     <div className="App bg-purple-200">
       {
-        currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <SignUp onFormSwitch={toggleForm} />
+        // currentForm === "login" ? <Login onFormSwitch={toggleForm} /> : <SignUp onFormSwitch={toggleForm} />
       }
     </div>
+    </Footer>
+    </NavBar>
   );
 }
 
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -12,7 +12,7 @@ function NavBar() {
           <li><a href='#home'>Home</a></li>
           <li><a href='#about'>About</a></li>
           <li><a href='#contact'>Contact Us</a></li>
-          <li><a href='#beneficiaries'>Beneficiaries</a></li>
+          <li><a href='#beneficiaries'>Beneficiary Stories</a></li>
         </ul>
         <div className='navbar-buttons'>
           <button className='donate-button'>Donate</button>
