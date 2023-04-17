91e8f26de83ce0dbbf13962c53523271e5b9a75d
Joanna <astronautnemo@gmail.com>
2023-04-17 14:38:50 +0300

    created the Donor Mailer

---
+++ Backend/app/mailers/donor_notifier_mailer.rb
@@ -0,0 +1,2 @@
+class DonorNotifierMailer < ApplicationMailer
+end
---
+++ Backend/test/mailers/previews/donor_notifier_mailer_preview.rb
@@ -0,0 +1,4 @@
+# Preview all emails at http://localhost:3000/rails/mailers/donor_notifier_mailer
+class DonorNotifierMailerPreview < ActionMailer::Preview
+
+end
