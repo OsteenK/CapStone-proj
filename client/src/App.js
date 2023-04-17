4361f5ab53e442a831c38e49fb8b4a9a3e9f8f1c
Glen <glen.kimani@student.moringaschool.com>
2023-04-17 16:44:46 +0300

    Navbar and Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
-  const [currentForm, setCurrentForm] = useState('login');
-
-  const toggleForm = (formName) => {
-    setCurrentForm(formName);
-  }
-
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      {/* The rest of your app */}
+      <Footer />
     </div>
   );
 }
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
       <div className='navbar-left'>
         <img src='give-hope-logo.png' alt='Give Hope Logo' />
       </div>
-      <div className='navbar-right'>
+      <div className='navbar-left'>
         <ul>
           <li><a href='#home'>Home</a></li>
           <li><a href='#about'>About</a></li>
