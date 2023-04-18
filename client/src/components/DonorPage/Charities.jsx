6d54d2abb4316200b266c402e1a8709153532226
osteen <osteen.mutinda@student.moringaschool.com>
2023-04-18 10:12:19 +0300

    Authentication implementation

--- client/src/components/DonorPage/Charities.jsx
+++ client/src/components/DonorPage/Charities.jsx
@@ -3,10 +3,13 @@ import "./Charities.css";
 
 
 const Charities = () => {
+  
+
   return (
-    <div>
+   <div>
       <NavBar/>
-        <section className='hero'>
+       
+      <section className='hero'>
         <div className='container1'>
         <div className='card'  style={{backgroundColor: 'rgba(136, 98, 219, 0.3)'}}>
         <h1>HELP MERCY</h1>
@@ -16,15 +19,17 @@ const Charities = () => {
         <button className='btn btn'style={{backgroundColor:'#9840D7',borderRadius:'20px',margin:'30px'}}>Donate Now!</button>
         </div>
         </div>
-        </section>
+       </section>
+     
+        
         <div className='hero2'>
         <h1>Active Charities</h1>
         <p className='text1'>Each of our carefully selected charities aim to promote equality<br></br>improve health outcomes and create a more just and equitable world for all,through proper sanitation and sanitary products.</p>
         </div>
         <div className='container'>
-  <div className='row'>
-    <div className='col-md-4'>
-      <div className='card'>
+       <div className='row'>
+       <div className='col-md-4'>
+       <div className='card'>
         <img className='card-img-top' src='https://images.unsplash.com/photo-1509099927777-1b8a87bc3b21?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MnwyNDU5NzU3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='...' />
         <div className='card-body'>
           <h5 className='card-title'>PadUp 4 North Eastern</h5>
@@ -32,8 +37,8 @@ const Charities = () => {
           <button className='btn btn-primary '>Donate Now</button>
         </div>
       </div>
-    </div>
-    <div className='col-md-4'>
+     </div>
+     <div className='col-md-4'>
       <div className='card'>
         <img className='card-img-top' src='https://images.unsplash.com/photo-1509100297676-1a18b3842dd6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8Mjh8MjQ1OTc1N3x8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60' alt='...' />
         <div className='card-body'>
@@ -42,8 +47,8 @@ const Charities = () => {
           <button className='btn btn-primary'>Donate Now</button>
         </div>
       </div>
-    </div>
-    <div className='col-md-4'>
+      </div>
+      <div className='col-md-4'>
       <div className='card'>
         <img className='card-img-top' src='https://images.unsplash.com/photo-1584012961499-2147c0b3cf0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3wyNDU5NzU3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='...' />
         <div className='card-body'>
@@ -52,8 +57,8 @@ const Charities = () => {
           <button className='btn btn-primary'>Donate Now</button>
         </div>
       </div>
-    </div>
-    <div className='col-md-4'>
+     </div>
+     <div className='col-md-4'>
       <div className='card'>
         <img className='card-img-top' src='https://images.unsplash.com/photo-1509099927777-1b8a87bc3b21?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MnwyNDU5NzU3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='...' />
         <div className='card-body'>
@@ -62,8 +67,8 @@ const Charities = () => {
           <button className='btn btn-primary '>Donate Now</button>
         </div>
       </div>
-    </div>
-    <div className='col-md-4'>
+      </div>
+      <div className='col-md-4'>
       <div className='card'>
         <img className='card-img-top' src='https://images.unsplash.com/photo-1509100297676-1a18b3842dd6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8Mjh8MjQ1OTc1N3x8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60' alt='...' />
         <div className='card-body'>
@@ -72,8 +77,8 @@ const Charities = () => {
           <button className='btn btn-primary'>Donate Now</button>
         </div>
       </div>
-    </div>
-    <div className='col-md-4'>
+     </div>
+     <div className='col-md-4'>
       <div className='card'>
         <img className='card-img-top' src='https://images.unsplash.com/photo-1584012961499-2147c0b3cf0d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8M3wyNDU5NzU3fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60' alt='...' />
         <div className='card-body'>
@@ -82,18 +87,22 @@ const Charities = () => {
           <button className='btn btn-primary'>Donate Now</button>
         </div>
       </div>
-    </div>
-    <div class="button-container">
-  <button class="bttn"> 1</button>
-  <button class="bttn"> 2</button>
-  <button class="bttn"> 3</button>
-</div>
-  </div>
-</div>
+      </div>
+      <div class="button-container">
+          <button class="bttn"> 1</button>
+             <button class="bttn"> 2</button>
+               <button class="bttn"> 3</button>
+      </div>
+     </div>
+     </div>
 
-    </div>
-  );
-};
+    
+   
+   </div>
+  
+ )
+  };
+
 
 export default Charities;
 
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -14,87 +14,52 @@ function NavBar(user) {
      navigate("/login");
   }
   return (
-    <nav className="navbar navbar-expand-lg shadow-md py-2  relative flex items-center w-full justify-between">
+    <nav className='navbar'>
       <div className='navbar-left'>
-        <img src='give-hope-logo.png' alt='Give Hope ' />
+        <img src='give-hope-logo.png' alt='Give Hope Logo' />
       </div>
-      <div className='navbar-left'>
+      {/* <div className="navbar-collapse collapse grow items-center" id="navbarSupportedContentY"> */}
+       <div className='navbar-left'>
         <ul>
-        
-      <div className="px-6 w-full flex flex-wrap items-center justify-between">
-        <div className="flex items-center">
-          <button
-            className="navbar-toggler border-0 py-3 lg:hidden leading-none text-xl bg-transparent text-gray-600 hover:text-gray-700 focus:text-gray-700 transition-shadow duration-150 ease-in-out mr-2"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContentY"
-            aria-controls="navbarSupportedContentY"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <svg
-              aria-hidden="true"
-              focusable="false"
-              data-prefix="fas"
-              className="w-5"
-              role="img"
-              xmlns=""
-              viewBox="0 0 448 512"
-            >
-              <path
-                fill=""
-                d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z"
-              ></path>
-            </svg>
-          </button>
           
-         <div id="navbarSupportedContentY">
-         <ul className="navbar-nav mr-auto lg:flex lg:flex-row">
-         
-         
+          
           <li>
-          <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-           <Link to="/home" className="nav-link active"> Home</Link> </a>   
-          
+              <Link to="/home" className="nav-link active">
+              Home
+                </Link>    
           </li>
           
           <li>
-          <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-            <Link to="/charities" className="nav-link active">About</Link></a>  
+              <Link to="/charities" className="nav-link active">
+              About
+              </Link>    
           </li>
-
           <li>
-          <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light">
-          <Link to="/ContactUs" className="nav-link active"> Contact Us</Link></a>    
+              <Link to="/contactus" className="nav-link active">
+                Contact Us
+                </Link>    
           </li>
-
           <li>
-          <a className="nav-link block pr-2 lg:px-2 py-2 text-gray-600 hover:text-gray-700 focus:text-gray-700 transition duration-150 ease-in-out" href="#!" data-mdb-ripple="true" data-mdb-ripple-color="light"> 
-          <Link to="/BeneficiariesS" className="nav-link active">Beneficiary Stories</Link></a>  
+              <Link to="/BeneficiariesS" className="nav-link active">
+              Beneficiary Stories
+              </Link>    
           </li>
           
-          
-         
-         </ul>
-         </div>
-      </div>
-        
+        </ul>
         <div className='navbar-buttons'>
           
-          <button type="button" className="inline-block px-6 py-2.5 mr-2 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">
-          <Link to="/charities" className="nav-link active">Donate</Link></button>
-          <button type="button" className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out" data-mdb-ripple="true" data-mdb-ripple-color="light">
-          <Link to="/login" className="nav-link active">Login</Link></button>
+
+          <Link   to='/charities' >
+          <button className='donate-button'>Donate</button>
+          </Link>
+          
+          <Link   to='/login' >
+          <button className='login-button'>Login</button>
+          </Link>
         </div>
-      
       </div>
-      
-      </ul>
-      </div>
-    
-    
-  </nav>
-  
+      {/* </div> */}
+    </nav>
   );
 }
 
