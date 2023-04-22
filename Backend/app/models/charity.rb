ca9dc0d7fa00b4af6451b64a9a748e4d003cd13b
Joanna <astronautnemo@gmail.com>
2023-04-22 09:33:57 +0300

    added model associations

--- Backend/app/models/charity.rb
+++ Backend/app/models/charity.rb
@@ -1,6 +1,7 @@
 class Charity < ApplicationRecord
     has_secure_password
     belongs_to :administrator
-  has_many :donations
-  has_many :beneficiaries
+    has_many :donations
+    has_many :donors, through: :donations
+    has_many :beneficiaries
 end
