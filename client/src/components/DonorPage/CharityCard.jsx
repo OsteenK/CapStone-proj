48ec8f2fa046cf0de7d0b8428f3ff90b776eef9b
Joanna <astronautnemo@gmail.com>
2023-04-23 13:50:07 +0300

    feature: added progress bar to cards

--- client/src/components/DonorPage/CharityCard.jsx
+++ client/src/components/DonorPage/CharityCard.jsx
@@ -18,9 +18,17 @@ function CharityCard({ charity }) {
                 <div className="max-h-32 overflow-hidden">
                     <p className='card-text text-base text-left'>{charity.description.slice(0, 200)}...</p>
                 </div>
-                              
+                
+                {/* Progress Bar */}
+                <div>
+                    <div className="h-3 w-full bg-lavender-100 rounded mt-4 mx-auto">
+                        <div className="h-3 bg-lavender-200 rounded" style={{width: (Math.ceil(charity.total_donations/charity.goal_amount*100))}}></div>
+                    </div>
+                    <span className="text-medium font-bold">Raised <span className="text-lavender-200">{charity.total_donations.toLocaleString("en-US", {style: "currency", currency: "USD", maximumFractionDigits: 0})}</span> of {charity.goal_amount.toLocaleString("en-US", {style: "currency", currency: "USD", maximumFractionDigits: 0})}</span>
+                </div>
+
               </div>
-              <div className="card-footer border-0 text-center bg-white my-4">
+              <div className="card-footer border-0 text-center bg-white mb-4">
                   <button className='bg-lavender-200 rounded-3xl p-2.5 px-4 h-12 text-white text-base font-bold bottom-0 hover:bg-lavender-300' onClick={handleClick}>Donate Now</button>
                 </div>
             </div>
