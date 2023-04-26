fb808be9d44be8c7bc3e0cda95ff588ab1dbe1e4
Joanna <astronautnemo@gmail.com>
2023-04-26 16:12:55 +0300

    feature: created Footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,25 +1,34 @@
-import React from 'react';
-import './Footer.css';
-// import GiveHopeLogo from './give-hope-logo.png';
+import { AiOutlineArrowRight } from "react-icons/ai";
 
 function Footer() {
     return (
       <footer>
-        <div className="logo-container ">
-          <img src="/give-hope-logo.png" alt="Give Hope Logo" />
-          <p>
-          Every member of Give Hope is dedicated to breaking down barriers to education for girls around the world. We can achieve this by not just providing sanitary towels, but also providing clean water and sanitation facilities.
+        <div className="bg-lavender-400">
+          <div className="container text-white text-center md:text-left mx-6 py-16 px-0">
+            <div className="grid-1 grid lg:gap-16 md:gap-12 md:grid-cols-1 lg:grid-cols-2">
+              <div className="text-left">
+                <img src="https://res.cloudinary.com/dtnt2f8ao/image/upload/v1682513605/Give%20Hope/Give%20Hope%20Logos/Give_Hope_Logo_dark_kdlh8a.png" className="pb-4 lg:h-32 md:h-28"/>
+                <p className="">
+                  Every member of Give Hope is dedicated to breaking down barriers to education for girls around the world. We can achieve this by not just providing sanitary towels, but also providing clean water and sanitation facilities.
+                  <br/><br/>
+                  <a className="text-lavender-200 font-bold">Join us</a> in the movement for gender equality. Every donation, big or small, can make a difference.
+                </p>
+              </div>
 
-Join us in the movement for gender equality. Every donation, big or small, can make a difference.
-            <a href="/join-us">Join us</a>
-          </p>
-        </div>
-        <div className="contact-container">
-          <h3>Contact Us</h3>
-          <p>Email: info@givehope.org</p>
-          <p>Telephone: 555-555-5555</p>
-          <p>Address: 123 Main Street, Anytown USA</p>
-          <a href="/get-in-touch">Get in touch</a>
+              <div className="text-left">
+                <h3 className="text-white">Contact Us</h3>
+                <p>Email: givehopecharities@gmail.com</p>
+                <br/>
+                <p>Telephone:<br/>+254-7123-456-89<br/>+254-7987-654-32 </p>
+                <br/>
+                <p>Address: This Street, This Building, This Room</p>
+                <br/>
+                <a className="text-white flex gap-1 items-center" href="/contactus">Get in Touch <AiOutlineArrowRight/></a>
+              </div>
+            </div>
+          </div>
+
+          
         </div>
       </footer>
     );
